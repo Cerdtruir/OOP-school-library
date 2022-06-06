bc7e35838fa5f14952f8ac779a883842b9d03f80
Cerdtruir <ghoornabeel@gmail.com>
2022-06-06 18:06:25 +0200

    Add add_person functionality

--- main.rb
+++ main.rb
@@ -51,6 +51,53 @@ class SchoolLibrary
     puts 'The book has been successfully added'
     main
   end
+
+  def choose_person_type
+    puts 'Please select the type of person you would like to add: '
+    puts '1 - Student'
+    puts '2 - Teacher'
+    person_type = gets.chomp
+    add_person(person_type)
+  end
+
+  def add_person(person_type)
+    print 'Name: '
+    name = gets.chomp
+    print 'Age: '
+    age = gets.chomp
+    case person_type
+    when '1'
+      add_student(name, age)
+    when '2'
+      add_teacher(name, age)
+    else
+      puts 'Invalid input'
+      main
+    end
+    puts
+    puts "Successfully added #{name}"
+  end
+
+  def add_student(name, age)
+    print 'Does the student have parental permission? [Y/N]: '
+    permission = gets.chomp
+    case permission.upcase
+    when 'Y'
+      p permission = true
+    when 'N'
+      p permission = false
+    else
+      puts 'Invalid input'
+      main
+    end
+    people << Student.new(age: age, name: name, parent_permission: permission)
+  end
+
+  def add_teacher(name, age)
+    print 'Specialization:'
+    specialization = gets.chomp
+    people << Teacher.new(age: age, name: name, specialization: specialization)
+  end
 end
 
 school_library = SchoolLibrary.new
