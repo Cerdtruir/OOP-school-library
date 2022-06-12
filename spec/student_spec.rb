fbbbc3084f180e5ef38666f3d234dfec5e7456ae
Cerdtruir <ghoornabeel@gmail.com>
2022-06-12 20:21:35 +0200

    Fix failing rental test and fix descriptions

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -7,13 +7,14 @@ describe Student do
     expect(Student).to be < Person
   end
   student = Student.new(age: 0, name: 'name', parent_permission: true)
-  context 'Add a student' do
+  context 'Do student methods work' do
     it 'Method play hooky working?' do
       expect(student.play_hooky).to eq "¯\(ツ)/¯"
     end
-    it 'Does the book have the added classsroom' do
-      student.classroom = 'asd'
-      expect(classroom0.last).to eq student
+    it 'Does the student have the added classsroom' do
+      classroom0 = Classroom.new('0')
+      student.classroom = classroom0
+      expect(classroom0.students.last).to eq student
     end
   end
 end
