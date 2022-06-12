8b443ad8545ce56a6bd02df7609f5bd619709588
Cerdtruir <ghoornabeel@gmail.com>
2022-06-12 15:30:42 +0200

    Remove useless code

--- main.rb
+++ main.rb
@@ -6,20 +6,21 @@ class SchoolLibrary
     @people = []
   end
 
-  def main_menu
-    puts 'Welcome to the School Library App'
-    puts 'Please choose an option by entering one of the following numbers:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Add a person'
-    puts '4 - Add a book'
-    puts '5 - Add a rental'
-    puts '6 - List books rented by a person'
-    puts '7 - Exit'
-    selection = gets.chomp
-    puts ''
-    select_menu(selection)
-  end
+# rubocop:disable Metrics/MethodLength
+def main_menu
+  puts 'Welcome to the School Library App'
+  puts 'Please choose an option by entering one of the following numbers:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Add a person'
+  puts '4 - Add a book'
+  puts '5 - Add a rental'
+  puts '6 - List books rented by a person'
+  puts '7 - Exit'
+  selection = gets.chomp
+  puts
+  select_menu(selection)
+end
 
   private
 
